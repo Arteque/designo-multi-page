b5310ba0850c684ad37eafe679c145cc6e8138f1
Factum <art3que@gmail.com>
2024-03-21 23:20:06 +0100

    about page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -100,6 +100,8 @@ function AboutUs() {
                 </MediumHeading>
                 <p>
                 As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.
+                <br />
+                <br />  
                 We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.
                 </p>
              </div>
